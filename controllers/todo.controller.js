1f741edc3a92b99483fe6d6a1bf00f2808316913
Yaroslava Nahorna <yasya74800@gmail.com>
2026-04-06 12:00:00 +0300

    Create error handling for unit and integration tests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,8 +1,12 @@
 const TodoModel = require('../models/todo.model')
 
 const createTodo = async (req, res, next) => {
-    const createdModel = await TodoModel.create(req.body)
-    res.status(201).json(createdModel)
+   try {
+        const createdModel = await TodoModel.create(req.body);
+        res.status(201).json(createdModel)
+    } catch(error){
+        next(error)
+    }
 } 
 
 module.exports = {createTodo} 
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -34,4 +34,11 @@ describe('TodoController.createTodo', () => {
         await TodoController.createTodo(req, res, next)
         expect(res._getJSONData()).toStrictEqual(newTodo)
     }) 
+    it('should handle errors', async () => {
+        const errorMessage = { message: "Done property missing"};
+        const rejectPromise = Promise.reject(errorMessage);
+        TodoModel.create.mockReturnValue(rejectPromise);
+        await TodoController.createTodo(req, res, next);
+        expect(next).toBeCalledWith(errorMessage);
+    })
 })
